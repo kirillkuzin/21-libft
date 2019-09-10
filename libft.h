0132d720caf48c06b8762d7d2d0abb7b3d74c9dc
Greenbeard Geordi <ggeordi@ox-p4.21-school.ru>
2019-09-10 20:42:49 +0300

    Initial commit

---
+++ libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggeordi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/10 17:14:11 by ggeordi           #+#    #+#             */
+/*   Updated: 2019/09/10 20:17:54 by ggeordi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+void	*ft_memset(void *destination, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *destination, void *source, size_t n);
+
+#endif
